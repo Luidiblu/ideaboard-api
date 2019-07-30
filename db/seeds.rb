423f48aa63977ccb81db05689f915937c641c0de
Diego Pisani <diego.luidiblu@gmail.com>
2019-07-30 12:24:54 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ideas = Idea.create(
+  [
+    {
+      title: "Elevators",
+      body: "that just vaporize anyone who tries walking in before people inside have stepped out, without exception."
+    },
+    {
+      title: "TV",
+      body: "that automatically changes text notification sounds in shows and commercials to something different than the one on your phone."
+    },
+    {
+      title: "Glass doors",
+      body: "that quickly become opaque when detecting someone is about to walk into it."
+    },
+    {
+      title: "A pie shop",
+      body: "that sells slices that are exactly 90 degrees called “Pie-Thagorous."
+    }
+  ])
