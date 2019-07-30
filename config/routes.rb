2b6a7d180583898cff7abf315273691aa17c5fd7
Diego Pisani <diego.luidiblu@gmail.com>
2019-07-30 13:08:16 -0300

    Rails Wars: Episode VIII - The Last Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'ideas/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :ideas
+    end
+  end
 end
